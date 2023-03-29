2ab2d5b1b538f15780904d2ca73604c4c1f572d6
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-03-29 12:58:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ function saturdayFun(activity = "roller-skate") {
 function mondayWork(activity='go to the office'){
     return `This Monday, I will ${activity}.`
 }
-mondayWork()
 
-
-
-
+function wrapAdjective(parameter1 = "*") {
+    return function (parameter2 = "special") {
+        return `You are ${parameter1}${parameter2}${parameter1}!`
+    }
+}
+console.log(wrapAdjective("||")("a dedicated programmer"))
 
 
 
